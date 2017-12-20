77392aac7dcd0de98c4f4178e3bd67e7bbc513f0
mellon <eason773@sina.com>
2017-12-21 07:51:45 +0800

    test

---
+++ src/main/java/com/manage/HelloController.java
@@ -0,0 +1,17 @@
+package com.manage;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by melon on 2017/12/20.
+ */
+@RestController
+public class HelloController {
+
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String hello(){
+		return "hello~";
+	}
+}
