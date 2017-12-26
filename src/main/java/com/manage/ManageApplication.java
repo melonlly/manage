99eb81b16c3bdc183069a407a160c2211589f0a3
HangSun <598159187@qq.com>
2017-12-26 14:38:19 +0800

    程序启动入口增加标注性说明

--- src/main/java/com/manage/ManageApplication.java
+++ src/main/java/com/manage/ManageApplication.java
@@ -5,10 +5,15 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//@Configuration ：标明该类使用Spring基于Java的配置
+//@ComponentScan ：启用组件扫描
+//@EnableAutoConfiguration ：开启spring boot的自动配置
+//@SpringBootApplication 注解代表的是以上三个注解的组合
+@SpringBootApplication      //开启组件扫描和自动配置
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class ManageApplication {
 
+    //负责自动和引导应用程序
     public static void main(String[] args) {
         SpringApplication.run(ManageApplication.class, args);
     }
